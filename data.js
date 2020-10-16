b27432d13259088a34f03f7983750d79c66c7299
Khắc Kiên <kien.nk@teko.vn>
2020-10-16 17:45:18 +0700

    First look

---
+++ data.js
@@ -0,0 +1,7 @@
+const ICONS = [{ x: 100, y: 100, type: 'TELEPORT' }];
+
+const ICON_TYPE = {
+  TELEPORT: 'https://genshin-impact-map.appsample.com/images/marker-teleport.png',
+};
+
+module.exports = { ICONS, ICON_TYPE };
